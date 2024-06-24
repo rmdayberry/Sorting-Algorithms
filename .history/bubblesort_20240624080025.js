5f24fe28a8fa05a45f7b3ea393e1ead9b2e28db3
rmdayberry <dayberryrm@gmail.com>
2024-06-24 08:04:58 -0500

    Run Test cases
    Ran test cases for bubble sort function

---
+++ .history/bubblesort_20240624080025.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+//test cases
+bubbleSort([4, 20, 12, 10, 7, 9]);
