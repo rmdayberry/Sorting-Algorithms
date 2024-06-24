beb6c711a7f3ecc1becf8760dd5eb73063e7a7ed
rmdayberry <dayberryrm@gmail.com>
2024-06-24 07:57:07 -0500

    Create bubbleSort function file
    Create bubbleSort function JS file

---
+++ .history/bubblesort_20240624074918.js
@@ -0,0 +1,10 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+    }
+    return arr;
+  }
+}
