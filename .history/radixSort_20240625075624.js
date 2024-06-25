a275438779fa0bf880d8486bef687d1160eb3b77
rmdayberry <dayberryrm@gmail.com>
2024-06-25 08:57:40 -0400

    Create radixSort file and function
    Create radixSort file and function and test cases

---
+++ .history/radixSort_20240625075624.js
@@ -0,0 +1,36 @@
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let num of nums) {
+    maxDigits = Math.max(maxDigits, digitCount(num));
+  }
+  return maxDigits;
+}
+
+function radixSort(nums) {
+  const maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      const digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
+//Test cases
+radixSort([8, 6, 1, 12]);
+//[1, 6, 8, 12]
+radixSort([10, 100, 1, 1000, 10000000]);
+//(5) [1, 10, 100, 1000, 10000000]
+radixSort([902, 4, 7, 408, 29, 9637, 1556, 3556, 8157, 4386, 86, 593]);
+//
